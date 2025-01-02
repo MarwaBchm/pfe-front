6514e74f0de789d17d6501360f93c9ed63b6f261
MarwaBchm <168100326+MarwaBchm@users.noreply.github.com>
2025-01-02 12:20:51 -0800

    up5
    Master responsible wish list

--- pfe-frontend/src/components/wishList.jsx
+++ pfe-frontend/src/components/wishList.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ProfessorWishList from "./ui/ProfessorWishList";
 import StudentWishList from "./ui/StudentWishList";
 import AdminWishList from "./ui/AdminWishList";
+import ResponsibleWishList from "./ui/ResponsibleWishList";
+
 const WishList = () => {
-  // Define the role (Change 'professor' to 'student' or 'admin' to test)
-  const role = "admin"; // Change to 'student' to display StudentWishList
+  // Define the role (Change 'professor' to 'student' or 'admin' or 'responsible' to test)
+  const role = "responsible"; // Change to 'student' to display StudentWishList
 
   return (
     <div className=" flex justify-start items-start  w-full mx-2 my-3 rounded-md">
@@ -13,7 +15,7 @@ const WishList = () => {
       ) : role === "student" ? (
         <StudentWishList />
       ) : (
-        <AdminWishList />
+       role==="admin"? <AdminWishList/>:<ResponsibleWishList/>
       )}
     </div>
   );
