afd027153d104d1288adc89983ea554fbcc4bc89
MarwaBchm <168100326+MarwaBchm@users.noreply.github.com>
2025-01-02 11:44:31 -0800

    up3
     Admin Wish List .... still need somestuffs in the ui

--- pfe-frontend/src/components/wishList.jsx
+++ pfe-frontend/src/components/wishList.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ProfessorWishList from "./ui/ProfessorWishList";
 import StudentWishList from "./ui/StudentWishList";
-
+import AdminWishList from "./ui/AdminWishList";
 const WishList = () => {
-  // Define the role (Change 'professor' to 'student' to test)
-  const role = "professor"; // Change to 'student' to display StudentWishList
+  // Define the role (Change 'professor' to 'student' or 'admin' to test)
+  const role = "admin"; // Change to 'student' to display StudentWishList
 
   return (
     <div className=" flex justify-start items-start  w-full mx-2 my-3 rounded-md">
-      {role === "professor" ? <ProfessorWishList /> : <StudentWishList />}
+      {role === "professor" ? (
+        <ProfessorWishList />
+      ) : role === "student" ? (
+        <StudentWishList />
+      ) : (
+        <AdminWishList />
+      )}
     </div>
   );
 };
